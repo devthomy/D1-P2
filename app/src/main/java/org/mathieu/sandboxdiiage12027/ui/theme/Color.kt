f4e6908c9916f6be22446dde36f28ccd43d64236
mbarrand <matteo.barrand10@gmail.com>
2024-12-02 13:21:37 +0100

    Push début color

--- app/src/main/java/org/mathieu/sandboxdiiage12027/ui/theme/Color.kt
+++ app/src/main/java/org/mathieu/sandboxdiiage12027/ui/theme/Color.kt
@@ -1,11 +1,18 @@
 package org.mathieu.sandboxdiiage12027.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundColor : Color
+    @Composable get() = MaterialTheme.colorScheme.background
+val OnBackgroundColor : Color
+    @Composable get() = MaterialTheme.colorScheme.onBackground
+val PrimaryColor : Color
+    @Composable get() = MaterialTheme.colorScheme.primary
+val OnPrimaryColor : Color
+    @Composable get() = MaterialTheme.colorScheme.onPrimary
+val SecondaryColor : Color
+    @Composable get() = MaterialTheme.colorScheme.secondary
+val OnSecondaryColor : Color
+    @Composable get() = MaterialTheme.colorScheme.onSecondary
